88a31166e1665005a999620cc3f19b33e8fb443a
Eduardo Cárdenas <eduardocj777@gmail.com>
2026-05-22 15:59:15 -0600

    login y register

--- JS/login.js
+++ JS/login.js
@@ -1,20 +1,24 @@
+async function handleLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const API_URL = "https://calamity-hypertext-strenuous.ngrok-free.dev";
 
+    const test = await fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true"
+        },
+        body: JSON.stringify([username, password])
+    }); 
 
-  async function handleLogin(event) {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const API_URL = "https://calamity-hypertext-strenuous.ngrok-free.dev";
-        alert(`Intentando iniciar sesión con:\nCorreo: ${username}\nContraseña: ${password}`);
-        const test = await fetch(`${API_URL}/roles`, {
-            
-            headers: { 
-                "ngrok-skip-browser-warning": "true",
-                "Content-Type": "application/json"
-            },
+    const data = await test.json();
 
-        }); 
-        let data = await test.json();
-        console.log(data);
+    if (data.ok) {
 
-    } 
+        window.location.href = "/public/index.html";
+    } else {
+        alert("Usuario y/o Contraseña Incorrectos");
+    }
+}
--- JS/register.js
+++ JS/register.js
@@ -1,9 +1,79 @@
-function toggleCompanyFields() {
-            const userType = document.getElementById('user-type').value;
-            const companyForm = document.getElementById('info-para-empresa');
-            if (userType === 'empresa') {
-                companyForm.style.display = 'block';
-            } else {
-                companyForm.style.display = 'none';
-            }
+
+let userType;
+
+async function registrarUser(event) {
+    event.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const paterno = document.getElementById("last-name").value;
+    const materno = document.getElementById("middle-name").value;
+    const correo = document.getElementById("email").value;
+    const telefono = document.getElementById("phone").value;
+    const pass = document.getElementById("new-password").value;
+    const passconf = document.getElementById("confirm-password").value;
+    
+    const ciudad = document.getElementById("city").value;
+    const direccion = document.getElementById("address").value;
+
+
+    if (!nombre || !correo || !telefono || !pass || !ciudad || !direccion) {
+        alert("Por favor llena todos los campos obligatorios");
+        return;
+    }
+    if(pass !== passconf){
+        alert("Las contraseñas deben coincidir");
+        return;
+    }
+
+    const fechaRegistro = new Date().toISOString().split("T")[0];
+    let body;
+
+    if (userType === "empresa") {
+        const rfc = document.getElementById("rfc").value;
+        const razonSocial = document.getElementById("business-name").value;
+        const sectorIndustrial = document.getElementById("business-type").value;
+        const paginaWeb = document.getElementById("website").value;
+        const descripcion = document.getElementById("business-description").value;
+
+        if (!rfc || !razonSocial || !sectorIndustrial) {
+            alert("Por favor llena todos los campos de empresa");
+            return;
         }
+
+        body = [nombre, paterno, materno, 2, correo, telefono, pass, ciudad, direccion, fechaRegistro, rfc, razonSocial, sectorIndustrial, paginaWeb, descripcion];
+    } else {
+        body = [nombre, paterno, materno, 1, correo, telefono, pass, ciudad, direccion, fechaRegistro, null, null, null, null, null];
+    }
+
+    try {
+        const res = await fetch("https://calamity-hypertext-strenuous.ngrok-free.dev/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "ngrok-skip-browser-warning": "true"
+            },
+            body: JSON.stringify(body)
+        });
+
+        const data = await res.json();
+
+        if (res.ok) {
+            alert(data.message);
+            window.location.href = "../public/login.html";
+        } else {
+            alert(data.message);
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Error al conectar con el servidor");
+    }
+}
+
+function toggleCompanyFields() {
+    userType = document.getElementById('user-type').value;
+    const companyForm = document.getElementById('info-para-empresa');
+    if (userType === 'empresa') {
+        companyForm.style.display = 'block';
+    } else {
+        companyForm.style.display = 'none';
+    }
+}
