2217467042be11b23d19f32ecb97263f469b5612
Eduardo Cárdenas <eduardocj777@gmail.com>
2026-05-22 18:26:21 -0600

    dada

--- private/producer.js
+++ private/producer.js
@@ -186,6 +186,7 @@ async function handleFormSubmit(event) {
     const urlImagen = uploadedImageUrl;
     const latitud = currentCoords.latitude;
     const longitud = currentCoords.longitude;
+    const peso = document.getElementById('peso').value;
 
     const API_URL = "https://calamity-hypertext-strenuous.ngrok-free.dev";
 
@@ -197,7 +198,7 @@ async function handleFormSubmit(event) {
                 "Content-Type": "application/json",
                 "ngrok-skip-browser-warning": "true"
             },
-            body: JSON.stringify([idCategoria, idTipoPublicacion, idUsuarios, nombre, descripcion, cantidad, precio, unidadMedida, urlImagen, latitud, longitud])
+            body: JSON.stringify([idCategoria, idTipoPublicacion, idUsuarios, nombre, descripcion, cantidad, precio, unidadMedida, urlImagen, latitud, longitud, peso])
         });
 
         const data = await res.json(); // Se espera que el backend devuelva un objeto JSON
